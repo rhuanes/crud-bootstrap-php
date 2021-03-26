2bad5dc834cb27c3444ae6e6a7a94748b97bb80a
Rhuan <rhuan.limaa@hotmail.com>
2021-03-25 21:12:51 -0300

    Subindo para master

--- config.php
+++ config.php
@@ -1,25 +1,23 @@
-<?php	
+<?php
 
-/** O nome do banco de dados*/	
-define('DB_NAME', 'rhn_crud');	
+/** O nome do banco de dados*/
+define('DB_NAME', 'rhn_crud');
 
-/** Usuário do banco de dados MySQL */	
-define('DB_USER', 'root@localhost');	
+/** Usuário do banco de dados MySQL */
+define('DB_USER', 'root');
 
-/** Senha do banco de dados MySQL */	
-define('DB_PASSWORD', '');	
+/** Senha do banco de dados MySQL */
+define('DB_PASSWORD', '');
 
-/** nome do host do MySQL */	
-define('DB_HOST', 'localhost');	
+/** nome do host do MySQL */
+define('DB_HOST', 'localhost');
 
-/** caminho absoluto para a pasta do sistema **/	
-if ( !defined('ABSPATH') )
-		define('ABSPATH', dirname(C:\Users\rhuan\crud-bootstrap-php) . '/');			
+/** caminho absoluto para a pasta do sistema **/    if (!defined('ABSPATH'))        define('ABSPATH', dirname(__FILE__) . '/');
 
-/** caminho no server para o sistema **/	
-if ( !defined('BASEURL') )
-		define('BASEURL', 'http://localhost/phpmyadmin/index.php?route=/database/structure&db=rhn_crud');			
+/** caminho no server para o sistema **/    
+if (!defined('BASEURL'))        
+define('BASEURL', '/crud-bootstrap-php/');
 
-/** caminho do arquivo de banco de dados **/	
-if ( !defined('DBAPI') )
-		define('DBAPI', ABSPATH . 'C:\Users\rhuan\crud-bootstrap-php\inc');
+/** caminho do arquivo de banco de dados **/
+if (!defined('DBAPI'))
+    define('DBAPI', ABSPATH . 'inc/database.php');
